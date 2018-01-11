66b77fadbe3bce8d954d2b50464de1f1c9880573
Perki <pm@perki.com>
2018-01-11 13:41:55 +0100

    added dimension to lat / lon

--- SimplePryv.js
+++ SimplePryv.js
@@ -73,10 +73,12 @@
     }, {
       id: "lat",
       alias: "latitude",
+      columnRole: "dimension",
       dataType: tableau.dataTypeEnum.float
     }, {
       id: "lon",
       alias: "longitude",
+      columnRole: "dimension",
       dataType: tableau.dataTypeEnum.float
     }];
 
